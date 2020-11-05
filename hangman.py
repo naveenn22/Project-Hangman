083e1696bfb8f9195a23e1181e0ef94515e18176
naveenn22 <72121390+naveenn22@users.noreply.github.com>
2020-11-05 12:00:37 +0530

    Functions for hangman body parts

--- hangman.py
+++ hangman.py
@@ -9,24 +9,50 @@ def draw(canvas, width, height):
 
    
 # def face(canvas, width, height):
-    
+def Face(canvas, width, height):
+    canvas.create_oval(500,200,560,260,width=2)
+    canvas.create_line(517,220,519,222,width=3)
+    canvas.create_line(540,220,542,222,width=3)
+    canvas.create_line(529,230,529,235,width=3)
+    canvas.create_line(521,245,538,245,width=3)
 
-    canvas.create_oval(500, 200, 560, 260, width=2)
-    canvas.create_line(517, 220, 519, 222, width=3)     #left eye
-    canvas.create_line(540, 220, 542, 222, width=3)     #right eye
-    canvas.create_line(529, 230, 529, 235, width=3)     #nose
-    canvas.create_line(521, 245, 538, 245, width=3)     # mouth
-    canvas.create_line(530, 260, 530, 435, width=3)     #body line
-    canvas.create_line(470, 320, 530, 290, width=2)     #left hand
-    canvas.create_line(590, 320, 530, 290, width=2)     #right hand
-    canvas.create_line(470, 475, 530, 435, width=2)     #left leg
-    canvas.create_line(590, 475, 530, 435, width=2)
+def Body(canvas, width, height):
+    canvas.create_line(530,260,530,435,width=3)
+
+def lHand(canvas, width, height):
+    canvas.create_line(470,320,530,290,width=2) 
+
+def rHand(canvas, width, height):    
+    canvas.create_line(590,320,530,290,width=2)
+
+def lLeg(canvas, width, height):     
+    canvas.create_line(470,475,530,435,width=2)
+
+def rLeg(canvas, width, height):     
+    canvas.create_line(590,475,530,435,width=2)   
+# Hangman structure depending on guess counts
+    # canvas.create_oval(500, 200, 560, 260, width=2)
+    # canvas.create_line(517, 220, 519, 222, width=3)     #left eye
+    # canvas.create_line(540, 220, 542, 222, width=3)     #right eye
+    # canvas.create_line(529, 230, 529, 235, width=3)     #nose
+    # canvas.create_line(521, 245, 538, 245, width=3)     # mouth
+    # canvas.create_line(530, 260, 530, 435, width=3)     #body line
+    # canvas.create_line(470, 320, 530, 290, width=2)     #left hand
+    # canvas.create_line(590, 320, 530, 290, width=2)     #right hand
+    # canvas.create_line(470, 475, 530, 435, width=2)     #left leg
+    # canvas.create_line(590, 475, 530, 435, width=2)     #right leg
 
 def runDraw(width=300, height=300):
     root=Tk()
     canvas=Canvas(root,width=width,height=height)
     canvas.pack()
     draw(canvas,width,height)
+    Face(canvas,width,height)
+    Body(canvas,width,height)
+    lHand(canvas,width,height)
+    rHand(canvas,width,height)
+    lLeg(canvas,width,height)
+    rLeg(canvas,width,height)
     root.mainloop()
     print("bye")
 
